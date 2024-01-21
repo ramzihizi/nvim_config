9888288be035173d98df4f6124ba2ca5ac84de93
rHizi <ramzihizi@gmail.com>
2024-01-21 08:46:47 -0500

    reomoved mason ensure install lsp because they're not valid

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -8,9 +8,9 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
-        "typescript", 
-        "typescriptreact",
-        "typescript.tsx"
+        -- "typescript",  -> Not valid entry
+        -- "typescriptreact", -> Not valid entry
+        -- "typescript.tsx" -> Not valid entry
       })
     end,
   },
